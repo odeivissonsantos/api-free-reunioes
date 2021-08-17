9f9b99fa45602f093f22e31dcff441152d93b659
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-17 13:38:57 -0300

    inicializando o projeto

---
+++ src/main/java/com/odeivissonsantos/freereunioes/FreeReunioesAppApplication.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.freereunioes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeReunioesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeReunioesAppApplication.class, args);
+	}
+
+}
