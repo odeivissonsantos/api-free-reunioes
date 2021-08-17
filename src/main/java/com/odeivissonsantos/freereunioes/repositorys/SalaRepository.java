e4054b841866735d11ea25f6b572d3721aa57de1
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-17 14:23:04 -0300

    criado o repository da entidade

---
+++ src/main/java/com/odeivissonsantos/freereunioes/repositorys/SalaRepository.java
@@ -0,0 +1,9 @@
+package com.odeivissonsantos.freereunioes.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.odeivissonsantos.freereunioes.models.SalaModel;
+
+public interface SalaRepository extends JpaRepository<SalaModel, Integer> {
+
+}
